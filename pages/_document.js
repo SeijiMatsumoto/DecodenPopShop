6188d25b7769a7ebdca9c1651f88a9b31f1fe98a
SeijiMatsumoto <seijim27@gmail.com>
2022-07-26 18:03:00 -0400

    Add google fonts

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,31 @@ class MyDocument extends Document {
           <meta name="image" property="og:image" content="" />
           <link rel="icon" href="" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Mali&family=Roboto&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Mali&family=Titillium+Web&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz@8..144&display=swap"
+            rel="stylesheet"
+          ></link>
         </Head>
         <body>
           <Main />
