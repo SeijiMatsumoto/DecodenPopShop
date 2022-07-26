074aad0b67e1c2e0e122d7ca3ec5db39778d33c4
SeijiMatsumoto <seijim27@gmail.com>
2022-07-26 18:03:47 -0400

    Removed import

--- components/02. Body/01. Home/Body.tsx
+++ components/02. Body/01. Home/Body.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import styles from '../../../styles/02. Body/00. Home/Body.module.css';
 import Categories from './Categories';
 import Splash from './Splash';
 
 const Body = () => {
   return (
-    <div className={styles.bodyWrapper}>
+    <div>
       <Splash />
       <Categories />
     </div>
