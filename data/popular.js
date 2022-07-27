201695e3cd700a3d48a9eda181ead7c9267d3294
SeijiMatsumoto <seijim27@gmail.com>
2022-07-27 15:52:25 -0400

    added popular data

---
+++ data/popular.js
@@ -0,0 +1,20 @@
+export const popular = [
+  {
+    img: "https://m.media-amazon.com/images/I/511x6-86A6L.jpg",
+    title: "Duck on a Swing",
+    url: "/",
+    price: "$14.99",
+  },
+  {
+    img: "https://d1flfk77wl2xk4.cloudfront.net/Assets/GalleryImage/42/539/L_g0125753942.jpg",
+    title: "Quack Quack T-Shirt",
+    url: "/",
+    price: "$25.99",
+  },
+  {
+    img: "https://ae01.alicdn.com/kf/Haed30c62916c4282b4501f9e85e4b783V/Summer-sunlight-lovely-duck-phone-case-for-iphone-7-8-plus-12-mini-11-pro-max.jpg_Q90.jpg_.webp",
+    title: "Summer Duck iPhone 13 Case",
+    url: "/",
+    price: "$49.99",
+  },
+];
