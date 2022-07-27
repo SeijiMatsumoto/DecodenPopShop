c36b83d3fef40bafeda50a250106b273789ca5bd
Seiji Matsumoto <seijim27@gmail.com>
2022-07-27 09:39:33 -0400

    add random duck facts

---
+++ data/duckFacts.js
@@ -0,0 +1,176 @@
+export const duckFacts = [
+  {
+    heading: "Ducks Have Accents",
+    subtext:
+      "City ducks have a different accent than country ducks – typically louder!",
+  },
+  {
+    heading: "Ducklings Communicate Before Hatching",
+    subtext:
+      "Ducklings, like other waterfowl, learn to communicate with each other in the eggs and try to hatch at the same time.",
+  },
+  {
+    heading: "Ducks Have Great Vision",
+    subtext:
+      "Ducks have exceptional visuals and can see finer detail at farther distances than humans.",
+  },
+  {
+    heading: "Ducks Have Great Eyes",
+    subtext:
+      "Ducks can move each eye independently and they store information on the opposite sides of their brain.",
+  },
+  {
+    heading: "Ducks Can Sleep With One Eye Open",
+    subtext: "Ducks can sleep with one eye open to watch for predators.",
+  },
+  {
+    heading: "Duckbills Are Sensitive",
+    subtext:
+      "Duckbills are sensitive and have many touch receptors, making them similar to our fingertips and palms.",
+  },
+  {
+    heading: "Duck Bill Shape Have Purpose",
+    subtext:
+      "A duck’s bill varies by species and relates to its function. Flat bills are used to consume plant materials, while pointed bills are used to catch and eat fish.",
+  },
+  {
+    heading: "Ducks Can Swim in The Cold",
+    subtext:
+      "Ducks can swim when it’s cold out because the blood vessels in their feet are close together, preventing heat loss.",
+  },
+  {
+    heading: "Ducks Have Abstract Thought Capabilities",
+    subtext:
+      "Ducklings can understand the relationship between objects, which demonstrates abstract thought capabilities.",
+  },
+  {
+    heading: "Ducks Have a Favorite Color",
+    subtext:
+      "According to research, ducks may show a preference for colors in the green or blue spectrum.",
+  },
+  {
+    heading: "Ducks Are Full of Muscle",
+    subtext:
+      "Ducks, like other waterfowl, have as many as 12,000 separate muscles to control their feathers. They can use these to lift or compress feathers to dive underwater, show emotion, or regulate body heat.",
+  },
+  {
+    heading: "Ducks Have Great Self-defense Mechanisms",
+    subtext:
+      "Female ducks and ducklings have plain plumage with dark feathers that form a pattern on their head and eyes. This helps to camouflage their eyes, which could be seen by predators or pecked at by other ducks.",
+  },
+  {
+    heading: "Some Ducks Raise Two Broods in a Year",
+    subtext:
+      "Wood ducks are the only North American waterfowl that can raise two broods in one year. More than 11% of females may produce two broods each season.",
+  },
+  {
+    heading: "Ducks Eat Gold",
+    subtext:
+      "Waterfowl spawned the Gold Rush. Birds eat stone, gravel, and sand to grind up hard foods, which they store in their gizzard. The Gold Rush was spurred in Nebraska when hunters found gold nuggets in the gizzards of ducks.",
+  },
+  {
+    heading: "Ducks Practice Parasitism",
+    subtext:
+      "Several ducks and other waterfowl practice nest parasitism, which is when the female lays eggs in the nests of other females in the same species.",
+  },
+  {
+    heading: "Ducks Eat Acorns",
+    subtext:
+      "Wood ducks eat acorns. In a study conducted on captive wood ducks, the birds preferred willow oak acorns over acorns from other oak accords.",
+  },
+  {
+    heading: "Ducks May Have Eggs Fertilised by Different Males",
+    subtext:
+      "Mallards often have clutches that include eggs fertilized by different males, which biologists believe ensures successful fertilization and greater genetic variation.",
+  },
+  {
+    heading: "Ducks That Hatch Earlier Tend to Survive Longer",
+    subtext:
+      "In a study of breeding mallards conducted by the Canadian Wildlife Service, the ducklings that hatch in the first 5 days of the period account for 40% of the first-year hens that survived to breed the following year.",
+  },
+  {
+    heading: "Ducks Can Dive In Harsh Conditions",
+    subtext:
+      "Harlequin ducks nest in rocky crevices along streams and dive to the bottom of the roaring water to feed on invertebrates. When they’re done, they walk upstream.",
+  },
+  {
+    heading: "Ducks Can Dive Very Deep",
+    subtext:
+      "All ducks can dive, but some species are better at diving than others. The best is the long-tailed duck, also known as an oldsquaw, which can dive 240 feet.",
+  },
+  {
+    heading: "Ducks Gather Together",
+    subtext:
+      "In 1940, a U.S. Fish and Wildlife Service biologist observed the largest concentration of ducks surveyed from the air on Catahoula Lake in Louisiana. He claimed to see as many as 8 million ducks.",
+  },
+  {
+    heading: "Ducks Can Lay Large Eggs",
+    subtext:
+      "Ruddy ducks produce the largest eggs relative to their body size. Ruddy duck eggs can weigh more than the hen.",
+  },
+  {
+    heading: "Ducks Can Live Many Years",
+    subtext:
+      "The oldest duck to be taken by a hunter was a 29-year-old canvasback.",
+  },
+  {
+    heading: "Ducks Can Fly High",
+    subtext:
+      "Ducks migrate at an altitude of 200 to 4,000 feet but are capable of reaching heights of 21,000 feet.",
+  },
+  {
+    heading: "Ducks Are Concentrated",
+    subtext:
+      "Some of the highest densities of nesting ducks occur in the San Luis Valley of Colorado, where some habitats support as many as 1,000 breeding ducks per square mile.",
+  },
+  {
+    heading: "Ducks Are Fast",
+    subtext:
+      "The fastest duck recorded was a red-breasted merganser, which achieved an airspeed of 100 mph.",
+  },
+  {
+    heading: "Some Ducks Are Extinct",
+    subtext:
+      "The Labrador duck is the only known extinct North American waterfowl species, which is believed to have gone extinct in 1875 off Long Island, NY.",
+  },
+  {
+    heading: "Ducks Gather in Fissures on Ice",
+    subtext:
+      "The spectacled elder’s wintering grounds weren’t discovered until 1995, when researchers followed birds marked with satellite transmitters into the Bering Sea. Once there, they found a huge number of elders gathering in the fissures on the ice.",
+  },
+  {
+    heading: "Ducks Can Fly Far",
+    subtext:
+      "Fulvous whistling-ducks are found in Mexico and parts of the southern US, as well as southern Africa. It’s believed that the African population was carried to North America by strong winds.",
+  },
+  {
+    heading: "Ducks Are Flexible",
+    subtext:
+      "Severe weather can trigger a mass migration known as a “grand passage.” In 1995, a blizzard in the Prairie Pothole Region spurred a migration of millions of ducks and geese that jammed radar systems and ground flights in Nebraska and Missouri.",
+  },
+  {
+    heading: "Ducks Have Rained Down",
+    subtext:
+      "In 1973, hundreds of ducks rained down from the sky in Stuttgart, AK. The ducks were likely killed by hail, but some had ice on their wings that may have accumulated when the wind carried them to high altitudes. The ducks falling damaged cars and windows.",
+  },
+  {
+    heading: "Ducks Crossbreed in the Wild",
+    subtext:
+      "Hybridization doesn’t occur in the wild often, but mallards tend to crossbreed with 40 different waterfowl species. The wood duck takes the close second with crossbreeding with as many as 20 other duck species.",
+  },
+  {
+    heading: "All ducks have highly waterproof feathers",
+    subtext:
+      "A duck's feathers are so waterproof that even when the duck dives underwater, the downy underlayer of feathers right next to the skin will stay completely dry.",
+  },
+  {
+    heading: "Ducks are omnivorous",
+    subtext:
+      "Ducks will eat grass, aquatic plants, insects, seeds, fruit, fish, crustaceans and other types of food.",
+  },
+  {
+    heading: "Ducks have been domesticated",
+    subtext:
+      "Ducks are domesticated as pets and farm animals for more than 500 years, and all domestic ducks are descended from either the mallard or the Muscovy duck. Mallards, especially, are easy to crossbreed with other types of ducks, and mallards often hybridise with all types of ducks at local ponds.",
+  },
+];
