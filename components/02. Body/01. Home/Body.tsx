97d74b9c64de68034f7eae5c679ea9af3484c28c
SeijiMatsumoto <seijim27@gmail.com>
2022-07-27 15:51:29 -0400

    Added popular component

--- components/02. Body/01. Home/Body.tsx
+++ components/02. Body/01. Home/Body.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import DuckFacts from './DuckFacts';
 import Categories from './Categories';
 import Splash from './Splash';
+import Popular from './Popular';
 
 const Body = () => {
   return (
     <div>
       <Splash />
       <Categories />
+      <Popular />
       <DuckFacts />
     </div>
   );
