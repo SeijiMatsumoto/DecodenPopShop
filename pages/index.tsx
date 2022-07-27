da1f2c240b5058e33dd6d8467da37b26aa2950fb
SeijiMatsumoto <seijim27@gmail.com>
2022-07-27 17:31:51 -0400

    Add homepage tab title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import Body from '../components/02. Body/01. Home/Body';
 
 const Home: NextPage = () => {
   return (
-    <Body />
+    <>
+      <Head>
+        <title>Quack Goods</title>
+      </Head>
+      <Body />
+    </>
   )
 }
 
