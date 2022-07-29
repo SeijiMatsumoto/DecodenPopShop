5ab27bb19e0d7c2b4ee5ade129b7551dddf8ee83
SeijiMatsumoto <seijim27@gmail.com>
2022-07-29 18:19:12 -0400

    Remove styling

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,32 +1,9 @@
-import styled from 'styled-components';
-
-const _ = {
-  Wrapper: styled.div`
-    position: absolute;
-    top: 115px;
-    padding: 20px;
-    border: 1px solid red;
-    * {
-      font-family: 'Roboto Flex', sans-serif;
-    }
-    display: flex;
-    justify-content: center;
-  `,
-  InnerWrapper: styled.div`
-    width: 95%;
-    margin: 20px 0;
-    display: flex;
-    align-items: center;
-    flex-direction: row;
-  `
-}
+import Products from '../../components/02. Body/02. Products/Products';
 
 export default function index() {
   return (
-    <_.Wrapper>
-      <_.InnerWrapper>
-        Products
-      </_.InnerWrapper>
-    </_.Wrapper>
+    <div>
+      <Products />
+    </div>
   );
 };
