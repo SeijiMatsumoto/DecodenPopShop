c02de0333fbb08bb7d7c89f887b72eb0e992adc3
SeijiMatsumoto <seijim27@gmail.com>
2022-07-29 18:19:04 -0400

    Styling changes

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  position: absolute;
-  top: 115px;
+  position: relative;
+  top: 120px;
   padding: 20px;
   display: flex;
   justify-content: center;
