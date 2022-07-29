f94863d0e1525b019d76befb565339636cbe0889
SeijiMatsumoto <seijim27@gmail.com>
2022-07-29 14:03:57 -0400

    Change animations

--- components/02. Body/01. Home/Popular.tsx
+++ components/02. Body/01. Home/Popular.tsx
@@ -1,179 +1,169 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { SectionTitle } from '../../UILibrary';
 import { popular } from '../../../data/popular';
 import Link from 'next/link';
 import anime from 'animejs';
-const inView = require('in-view');
+import { checkInView } from '../../../helper/checkInView';
 
-const Wrapper = styled.div`
-  width: 100%;
-  margin: 100px 0 140px;
-  display: flex;
-  justify-content: center;
-  * {
-    font-family: 'Roboto', sans-serif;
-  }
+const Styles = {
+  Wrapper: styled.div`
+    width: 100%;
+    margin: 100px 0 140px;
+    display: flex;
+    justify-content: center;
+    * {
+      font-family: 'Roboto', sans-serif;
+    }
 
-  @media screen and (max-width: 500px) {
-    margin: 50px 0 30px;
-  }
-`;
-
-const InnerWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  width: 1320px;
-  flex-direction: column;
-`;
-
-const TitleWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  margin-bottom: 15px;
-  text-align: center;
-`;
-
-const BarAroundTitle = styled.span`
-  width: 30%;
-  height: 35%;
-  border-bottom: 1px solid #dadada;
-  margin: 0 30px;
-
-  @media screen and (max-width: 500px) {
-    width: 20%;
-  }
-`;
-
-const CardsWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  margin-top: 20px;
-
-  @media screen and (max-width: 1320px) {
+    @media screen and (max-width: 500px) {
+      margin: 50px 0 30px;
+    }
+  `,
+  InnerWrapper: styled.div`
+    display: flex;
+    align-items: center;
+    width: 1320px;
     flex-direction: column;
-  }
+  `,
+  TitleWrapper: styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin-bottom: 15px;
+    text-align: center;
+  `,
+  BarAroundTitle: styled.span`
+    width: 30%;
+    height: 35%;
+    border-bottom: 1px solid #dadada;
+    margin: 0 30px;
 
-  @media screen and (max-width: 400px) {
-    width: 95%;
-  }
-`;
-
-const CardWrapper = styled.div`
-  width: 380px;
-  cursor: pointer;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  opacity: 0;
-  position: relative;
-  top: 300px;
-  &:active {
-    transform: scale(0.99);
-    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
-  }
-
-  @media screen and (max-width: 1320px) {
-    margin-bottom: 50px;
-  }
-  @media screen and (max-width: 500px) {
-      opacity: 1;
-      position: relative;
-      top: 0;
+    @media screen and (max-width: 500px) {
+      width: 20%;
     }
-`;
+  `,
+  CardsWrapper: styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    margin-top: 20px;
 
-const FlipContainer = styled.div`
-  position: relative;
-
-  &:hover {
-    section {
-      transform: rotateX(-180deg);
-      transition-delay: 0.3s;
+    @media screen and (max-width: 1320px) {
+      flex-direction: column;
     }
-  }
-`;
 
-const Flipper = styled.section`
-  transition: 0.6s;
-	transform-style: preserve-3d;
-	position: relative;
-  transform-origin: 100% 190px;
-`;
-
-const CardFront = styled.div`
-  backface-visibility: hidden;
-  text-align: center;
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 2;
-	transform: rotateX(0deg);
-`;
-
-const CardImg = styled.img`
-  border-radius: 5px;
-  box-shadow: 0 10px 30px -10px rgb(0 0 0 / 10%);
-  height: 380px;
-  width: 380px;
-
-`;
-
-const CardBack = styled.div`
-  border-radius: 5px;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-  transform: rotateX(180deg);
-  background-color: #FFF5F0;
-  width: 380px;
-  height: 380px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const CardBackInner = styled.div`
-  width: 250px;
-  height: 250px;
-  box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 8px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  position: relative;
-  background-color: white;
-
-  &:hover {
-    a {
-      color: #fa741a;
+    @media screen and (max-width: 400px) {
+      width: 95%;
     }
-  }
-`;
+  `,
+  CardWrapper: styled.div`
+    width: 380px;
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    opacity: 0;
+    position: relative;
+    top: 300px;
+    &:active {
+      transform: scale(0.99);
+      box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+    }
 
-const ProductTitle = styled.span`
-  font-weight: bold;
-  font-size: 18px;
-  margin: 5px;
-  text-align: center;
-`;
+    @media screen and (max-width: 1320px) {
+      margin-bottom: 50px;
+    }
+    @media screen and (max-width: 500px) {
+        opacity: 1;
+        position: relative;
+        top: 0;
+      }
+  `,
+  FlipContainer: styled.div`
+    position: relative;
 
-const ProductPrice = styled.span``;
+    &:hover {
+      section {
+        transform: rotateX(-180deg);
+        transition-delay: 0.3s;
+      }
+    }
+  `,
+  Flipper: styled.section`
+    transition: 0.6s;
+    transform-style: preserve-3d;
+    position: relative;
+    transform-origin: 100% 190px;
+  `,
+  CardFront: styled.div`
+    backface-visibility: hidden;
+    text-align: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    transform: rotateX(0deg);
+  `,
+  CardImg: styled.img`
+    border-radius: 5px;
+    box-shadow: 0 10px 30px -10px rgb(0 0 0 / 10%);
+    height: 380px;
+    width: 380px;
 
-const ProductLink = styled.a`
-  text-decoration: none;
-  transition: all 0.3s;
-  margin-top: 5px;
-`;
+  `,
+  CardBack: styled.div`
+    border-radius: 5px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    transform: rotateX(180deg);
+    background-color: #FFF5F0;
+    width: 380px;
+    height: 380px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+  CardBackInner: styled.div`
+    width: 250px;
+    height: 250px;
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    position: relative;
+    background-color: white;
+
+    &:hover {
+      a {
+        color: #fa741a;
+      }
+    }
+  `,
+  ProductTitle: styled.span`
+    font-weight: bold;
+    font-size: 18px;
+    margin: 5px;
+    text-align: center;
+  `,
+  ProductPrice: styled.span``,
+  ProductLink: styled.a`
+    text-decoration: none;
+    transition: all 0.3s;
+    margin-top: 5px;
+  `
+}
 
 const Popular = () => {
+  const [isInView, setIsInView] = useState<boolean>(false);
+
   const animateIn = (selectors) => {
     var tl = anime.timeline({
-      easing: 'easeInOutElastic',
-      duration: 1000,
+      easing: 'linear',
+      duration: 500,
     });
 
     tl
@@ -186,69 +176,67 @@ const Popular = () => {
         targets: selectors[1],
         translateY: -300,
         opacity: 1,
-      }, '-=800')
+      }, '-=300')
       .add({
         targets: selectors[2],
         translateY: -300,
         opacity: 1,
-      }, '-=800')
+      }, '-=300')
   }
 
-  const checkInView = () => {
-    inView('#popular-title')
-      .on('enter', el => {
-        console.log('in view')
-        animateIn(['#popular-0', '#popular-1', '#popular-2']);
-      });
+  const scrollHandler = () => {
+    if (!isInView) checkInView('#popular-title', setIsInView);
   };
+
+  useEffect(() => {
+    if (isInView) animateIn(['#popular-0', '#popular-1', '#popular-2']);
+  }, [isInView])
 
   useEffect(() => {
     if (document) {
       const width = window.innerWidth;
       if (width > 500) {
-        console.log('Width:', width);
-        document.body.addEventListener('scroll', checkInView);
-        return () => document.body.removeEventListener('scroll', checkInView);
+        document.body.addEventListener('scroll', scrollHandler);
+        return () => document.body.removeEventListener('scroll', scrollHandler);
       }
     }
   }, []);
 
   return (
-    <Wrapper>
-      <InnerWrapper>
-        <TitleWrapper id="popular-title">
-          <BarAroundTitle />
+    <Styles.Wrapper>
+      <Styles.InnerWrapper>
+        <Styles.TitleWrapper>
+          <Styles.BarAroundTitle id="popular-title" />
           <SectionTitle text="Popular Products" />
-          <BarAroundTitle />
-        </TitleWrapper>
-        <CardsWrapper>
+          <Styles.BarAroundTitle />
+        </Styles.TitleWrapper>
+        <Styles.CardsWrapper>
           {popular.map((product, i) => {
             return (
               <Link href={product.url} key={product.img + '-popular'}>
-                <CardWrapper id={`popular-${i}`}>
-                  <FlipContainer>
-                    <Flipper>
-                      <CardFront>
-                        <CardImg src={product.img} alt="product" />
-                        <ProductTitle>{product.title}</ProductTitle>
-                      </CardFront>
-                      <CardBack>
-                        <CardBackInner>
-                          <ProductTitle>{product.title}</ProductTitle>
-                          <ProductPrice>{product.price}</ProductPrice>
-                          <ProductLink>View Product</ProductLink>
-                        </CardBackInner>
-                      </CardBack>
-                    </Flipper>
-                  </FlipContainer>
-                </CardWrapper>
+                <Styles.CardWrapper id={`popular-${i}`}>
+                  <Styles.FlipContainer>
+                    <Styles.Flipper>
+                      <Styles.CardFront>
+                        <Styles.CardImg src={product.img} alt="product" />
+                        <Styles.ProductTitle>{product.title}</Styles.ProductTitle>
+                      </Styles.CardFront>
+                      <Styles.CardBack>
+                        <Styles.CardBackInner>
+                          <Styles.ProductTitle>{product.title}</Styles.ProductTitle>
+                          <Styles.ProductPrice>{product.price}</Styles.ProductPrice>
+                          <Styles.ProductLink>View Product</Styles.ProductLink>
+                        </Styles.CardBackInner>
+                      </Styles.CardBack>
+                    </Styles.Flipper>
+                  </Styles.FlipContainer>
+                </Styles.CardWrapper>
               </Link>
             )
           })}
-
-        </CardsWrapper>
-      </InnerWrapper>
-    </Wrapper>
+        </Styles.CardsWrapper>
+      </Styles.InnerWrapper>
+    </Styles.Wrapper>
   );
 };
 
