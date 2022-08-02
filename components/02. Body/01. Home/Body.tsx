9fb4011a2369786c1dc44f364976fdc33d01641b
SeijiMatsumoto <seijim27@gmail.com>
2022-08-01 21:19:24 -0400

    Set current page when load

--- components/02. Body/01. Home/Body.tsx
+++ components/02. Body/01. Home/Body.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import DuckFacts from './DuckFacts';
 import Categories from './Categories';
 import Splash from './Splash';
 import Popular from './Popular';
 import Details from './Details';
+import { SettingsContext } from '../../Contexts/SettingsContext';
 
 const Body = () => {
+  const { setCurrentPage } = useContext(SettingsContext);
+
+  useEffect(() => {
+    setCurrentPage('home');
+  }, [])
+
   return (
     <div>
       <Splash />
