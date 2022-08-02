b1779ffcbf8298792a27ca522f2b6a039f09b61b
SeijiMatsumoto <seijim27@gmail.com>
2022-08-01 22:02:16 -0400

    Change current page context on component load

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import styled from 'styled-components';
+import { SettingsContext } from '../components/Contexts/SettingsContext'
 
 const Wrapper = styled.div`
-  position: relative;
-  top: 120px;
   padding: 20px;
   display: flex;
   justify-content: center;
 `;
 
 const Contact = () => {
+  const { setCurrentPage } = useContext(SettingsContext);
+
+  useEffect(() => {
+    setCurrentPage('contact');
+  }, [])
+
   return (
     <Wrapper>
       Contact
