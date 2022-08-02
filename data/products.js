3ae232389f0867d8178dc73b74544bcc000d9a82
SeijiMatsumoto <seijim27@gmail.com>
2022-08-01 21:18:12 -0400

    Added dummy products

---
+++ data/products.js
@@ -0,0 +1,54 @@
+export const products = {
+  cases: [
+    {
+      title: "Case",
+      src: "http://cdn.shopify.com/s/files/1/0568/3019/7968/products/O1CN01i7dLWY1SDbCkkSJ3H__1592762213.jpg_400x400_09aca2c7-c62b-4f32-8a28-b53747891a27.jpg?v=1640240526",
+      price: "$49.99",
+    },
+    {
+      title: "Case",
+      src: "https://media.karousell.com/media/photos/products/2020/12/4/customisable_decoden_phone_cas_1607108129_35f3df06_progressive.jpg",
+      price: "$49.99",
+    },
+    {
+      title: "Case",
+      src: "https://i.ebayimg.com/images/g/OtMAAOSwm9dgRQT3/s-l500.jpg",
+      price: "$49.99",
+    },
+    {
+      title: "Case",
+      src: "https://i.pinimg.com/originals/ce/ab/56/ceab56772794972e4740085fd7ec275e.jpg",
+      price: "$49.99",
+    },
+    {
+      title: "Case",
+      src: "https://i.pinimg.com/originals/c5/98/c1/c598c16a4ec838410769833f06d206a9.jpg",
+      price: "$49.99",
+    },
+    {
+      title: "Case",
+      src: "https://www.decodencase.com/wp-content/uploads/2021/01/il_1588xN.2817478491_4qrd.jpg",
+      price: "$49.99",
+    },
+    {
+      title: "Case",
+      src: "https://i.pinimg.com/originals/5e/9d/ba/5e9dba01d8c90f8751c8ec12a12f33dd.jpg",
+      price: "$49.99",
+    },
+    {
+      title: "Case",
+      src: "https://bestofkawaii.com/wp-content/uploads/2021/11/3d-princess-decoden-phone-case-cases-aladdin-aladin-alice-wonderland-baby-boy-ddlg-playground-334.jpg",
+      price: "$49.99",
+    },
+    {
+      title: "Case",
+      src: "https://i.redd.it/zb2x2meahff01.jpg",
+      price: "$49.99",
+    },
+    {
+      title: "Case",
+      src: "https://i.pinimg.com/736x/ec/f3/9c/ecf39ca23b8e728877893ab8d3770e8d.jpg",
+      price: "$49.99",
+    },
+  ],
+};
