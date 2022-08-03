dd737d52a005dbd2e50897a0ee8b1a1db9ad9a7a
SeijiMatsumoto <seijim27@gmail.com>
2022-08-03 08:19:16 -0400

    Add cloudbg css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 import '../styles/00. Global/global.css';
 import '../styles/00. Global/cloudButton.css';
+import '../styles/00. Global/cloudBG.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
