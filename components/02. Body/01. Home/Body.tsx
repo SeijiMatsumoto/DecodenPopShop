a79d74aa4f11f8a31f3a12a29a94c486403e6f9e
SeijiMatsumoto <seijim27@gmail.com>
2022-08-03 08:18:40 -0400

    Removed duck facts

--- components/02. Body/01. Home/Body.tsx
+++ components/02. Body/01. Home/Body.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useContext } from 'react';
-import DuckFacts from './DuckFacts';
 import Categories from './Categories';
 import Splash from './Splash';
 import Popular from './Popular';
@@ -18,7 +17,6 @@ const Body = () => {
       <Splash />
       <Categories />
       <Popular />
-      {/* <DuckFacts /> */}
       <Details />
     </div>
   );
