fcba2a26383ff6c32e32a1e419237e3d11895b6d
SeijiMatsumoto <seijim27@gmail.com>
2022-08-10 10:27:22 -0400

    Create page

---
+++ pages/products/all.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Products from '../../components/02. Body/02. Products/ProductsMain';
+
+const AllProducts = () => {
+
+  return (
+    <Products />
+  );
+};
+
+export default AllProducts;
