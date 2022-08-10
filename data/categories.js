20c0de44d6d0e5911468b58eda3f6bf94fa4fb91
SeijiMatsumoto <seijim27@gmail.com>
2022-08-10 10:26:23 -0400

    Update data

--- data/categories.js
+++ data/categories.js
@@ -1,1 +1,27 @@
-export const categories = ["Phone Cases", "Hair Clips", "Car Accessories"];
+export const categories = [
+  {
+    name: "Cream Glue Phone Cases",
+    img: "https://i.imgur.com/h0PkCmp.jpg",
+    url: "/products",
+  },
+  {
+    name: "Epoxy Phone Cases",
+    img: "http://cdn.shopify.com/s/files/1/0019/4776/2747/products/Washing-Duck-Transparent-Cute-Phone-Cases-For-iPhone-13-11-12-Pro-Max-Xs-Max-XR-7-8-Plus-7Plus-Touchy-Style.jpg?v=1638386369",
+    url: "/products",
+  },
+  {
+    name: "Hair Clips",
+    img: "https://gw.alicdn.com/i4/2211765499208/O1CN01SNlDjI2HtJgi0hUT4_!!0-item_pic.jpg_Q75.jpg_.webp",
+    url: "/products",
+  },
+  {
+    name: "Car Accessories",
+    img: "https://m.media-amazon.com/images/I/511x6-86A6L.jpg",
+    url: "/products",
+  },
+  {
+    name: "Other",
+    img: "https://i.pinimg.com/originals/3f/ab/ce/3fabce918acb4fd31ff3e4c3eaf9396e.jpg",
+    url: "/products",
+  },
+];
--- data/collections.js
+++ data/collections.js
@@ -1,1 +1,27 @@
-export const collections = ["PopMart", "zZton", "Rico", "Sanori", "Anime"];
+export const collections = [
+  {
+    name: "PopMart",
+    img: "https://cdn.shopify.com/s/files/1/0339/7091/3412/products/2_3e736b6a-5b56-4bf9-bd7f-aca163c42c03_750x750.jpg?v=1642583289",
+    url: "/products",
+  },
+  {
+    name: "zZton",
+    img: "https://bbts1.azureedge.net/images/p/full/2021/11/ea12096a-20b3-4c4d-afc9-f77ed8faea4c.jpg",
+    url: "/products",
+  },
+  {
+    name: "Rico",
+    img: "https://cdn.shopify.com/s/files/1/0090/7732/5890/products/8f5af8952a3036c937ff72935e73bcbc_3840.jpg?v=1632232469",
+    url: "/products",
+  },
+  {
+    name: "Sanori",
+    img: "https://osssource.alizila.com/uploads/2022/07/Sanrio-Characters.png",
+    url: "/products",
+  },
+  {
+    name: "Anime",
+    img: "https://wallpaperaccess.com/full/8259463.png",
+    url: "/products",
+  },
+];
