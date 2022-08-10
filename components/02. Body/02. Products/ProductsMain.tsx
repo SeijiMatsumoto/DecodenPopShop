4346209cc1046f1f29393248a00406a7ab29ea42
SeijiMatsumoto <seijim27@gmail.com>
2022-08-10 10:29:17 -0400

    add query params

--- components/02. Body/02. Products/ProductsMain.tsx
+++ components/02. Body/02. Products/ProductsMain.tsx
@@ -1,19 +1,34 @@
 import React, { useContext, useEffect } from 'react';
+import { useRouter } from "next/router";
 import styled from 'styled-components';
 import { Banner } from '../../UILibrary';
 import Cards from './Cards';
-import SortBar from './SortBar';
+import SideBar from './SideBar';
 import { SettingsContext } from '../../Contexts/SettingsContext';
 import { scrollToTop } from '../../../helper/scrollToTop';
-import DropdownBar from './DropdownBar';
 
 const _ = {
   Wrapper: styled.div`
-    width: 100%;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `,
+  ContentsWrapper: styled.div`
+    width: 1320px;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: center;
+
+    @media screen and (max-width: 1400px) {
+      width: 95vw;
+    }
   `,
 }
 
 const Products = () => {
+  const { query } = useRouter();
   const { setCurrentPage } = useContext(SettingsContext);
 
   useEffect(() => {
@@ -21,12 +36,19 @@ const Products = () => {
     scrollToTop();
   }, [])
 
+  let text: any;
+  if (query.category) text = query.category;
+  else if (query.collection) text = query.collection;
+  else if (query.results) text = "Products";
+  else text = "All Products";
+
   return (
     <_.Wrapper>
-      <Banner text='All Products' />
-      <DropdownBar />
-      <SortBar />
-      <Cards />
+      <Banner text={text} />
+      <_.ContentsWrapper>
+        <SideBar query={query} />
+        <Cards />
+      </_.ContentsWrapper>
     </_.Wrapper>
   );
 };
