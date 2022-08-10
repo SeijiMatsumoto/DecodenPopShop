99f0e24c5d31ed776ce210090c24f4655471ca85
SeijiMatsumoto <seijim27@gmail.com>
2022-08-10 10:29:09 -0400

    Styling changes and No products display

--- components/02. Body/02. Products/Cards.tsx
+++ components/02. Body/02. Products/Cards.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
-import { products } from '../../../data/products';
 import Card from './Card';
+import { SettingsContext } from '../../Contexts/SettingsContext';
 
 const _ = {
   Wrapper: styled.div`
-    width: 100vw;
-    padding: 30px 0;
+    padding: 30px 0 20px;
     display: flex;
     justify-content: center;
     align-items: center;
+    width: 80%;
+    @media (max-width: 1250px) {
+      width: 70%;
+    }
+    @media (max-width: 850px) {
+      width: 55%;
+    }
+    @media (max-width: 550px) {
+      width: 95vw;
+    }
   `,
   InnerWrapper: styled.div`
     display: grid;
-    @media (min-width: 1000px) {
+    @media (min-width: 900px) {
       grid-template-columns: repeat(2, 1fr);
     }
-    @media (min-width: 1400px) {
+    @media (min-width: 1250px) {
       grid-template-columns: repeat(3, 1fr);
-    }
-    @media (min-width: 2100px) {
-      grid-template-columns: repeat(4, 1fr);
     }
 
     @media screen and (max-width: 450px) {
@@ -29,19 +35,50 @@ const _ = {
       align-items: center;
       flex-direction: column;
     }
-  `
+  `,
+  NoProductsWrapper: styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 60vh;
+    text-align: center;
+
+    @media screen and (max-width: 1200px) {
+      width: 90%;
+    }
+  `,
+  NoProductsText: styled.span`
+    padding: 20px 20%;
+    font-size: 36px;
+    font-family: 'Mali', cursive;
+    @media screen and (max-width: 1200px) {
+      font-size: 25px;
+      padding: 20px;
+    }
+  `,
+  ContactButton: styled.button``,
 }
 
 const Cards = () => {
+  const { productsToShow, selectedCategory } = useContext(SettingsContext);
+
   return (
     <_.Wrapper>
-      <_.InnerWrapper>
-        {products.cases.map(product => {
-          return (
-            <Card key={product.src} src={product.src} title={product.title} price={product.price} />
-          )
-        })}
-      </_.InnerWrapper>
+      {productsToShow.length ?
+        <_.InnerWrapper>
+          {productsToShow.map((product: any) => {
+            return (
+              <Card key={product.images[0]} src={product.images[0]} title={product.title} price={product.price} />
+            )
+          })}
+        </_.InnerWrapper>
+        :
+        <_.NoProductsWrapper>
+          <_.NoProductsText>There are no {selectedCategory.indexOf(" ") > -1 ? selectedCategory : selectedCategory + ' products '} currently available. Sorry! :( </_.NoProductsText>
+          <_.ContactButton>Contact us?</_.ContactButton>
+        </_.NoProductsWrapper>
+      }
     </_.Wrapper>
   );
 };
