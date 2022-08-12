b21a9f3dda9b13967f56f33016005102b49a25a4
SeijiMatsumoto <seijim27@gmail.com>
2022-08-11 22:03:39 -0400

    add dummy questions

---
+++ data/questions.js
@@ -0,0 +1,34 @@
+export const questions = [
+  {
+    q: "What is decoden?",
+    a: 'Decode derives from the japanese term "Decoration Denwa", where denwa means phone.',
+  },
+  {
+    q: "How can I request a custom item?",
+    a: 'Decode derives from the japanese term "Decoration Denwa", where denwa means phone.Decode derives from the japanese term "Decoration Denwa", where denwa means phone.Decode derives from the japanese term "Decoration Denwa", where denwa means phone.Decode derives from the japanese term "Decoration Denwa", where denwa means phone.',
+  },
+  {
+    q: "How long will take for an item to arrive?",
+    a: 'Decode derives from the japanese term "Decoration Denwa", where denwa means phone.',
+  },
+  {
+    q: "What is the return policy?",
+    a: 'Decode derives from the japanese term "Decoration Denwa", where denwa means phone.',
+  },
+  {
+    q: "Do you ship internationally?",
+    a: 'Decode derives from the japanese term "Decoration Denwa", where denwa means phone.',
+  },
+  {
+    q: "How can I purchase an item?",
+    a: 'Decode derives from the japanese term "Decoration Denwa", where denwa means phone.',
+  },
+  {
+    q: "Who is Shu?",
+    a: 'Decode derives from the japanese term "Decoration Denwa", where denwa means phone.',
+  },
+  {
+    q: "How do you make the phone cases?",
+    a: 'Decode derives from the japanese term "Decoration Denwa", where denwa means phone.',
+  },
+];
