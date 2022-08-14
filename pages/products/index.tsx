ec945efa8a4a8d0c3255e2e9151062c72534e09e
SeijiMatsumoto <seijim27@gmail.com>
2022-08-13 23:23:53 -0400

    remove div

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -2,8 +2,6 @@ import ProductsMain from '../../components/02. Body/02. Products/ProductsMain';
 
 export default function index() {
   return (
-    <div>
-      <ProductsMain />
-    </div>
+    <ProductsMain />
   );
 };
