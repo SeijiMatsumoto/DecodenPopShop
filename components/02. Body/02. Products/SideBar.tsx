dc3393979923eef331c00ceaf1b5c31cc9d792ef
SeijiMatsumoto <seijim27@gmail.com>
2022-08-16 22:02:51 -0400

    remove unused

--- components/02. Body/02. Products/SideBar.tsx
+++ components/02. Body/02. Products/SideBar.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
 import { categories } from '../../../data/categories';
 import { collections } from '../../../data/collections';
